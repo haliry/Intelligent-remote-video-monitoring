21503bcd662918c1a309763e219d867956f98e27
haliry <40397845+haliry@users.noreply.github.com>
2021-12-28 20:48:20 +0800

    运动检测模式

---
+++ 运动检测模式/Downloads/stream-video-browser/pyimagesearch/send_mail.py
@@ -0,0 +1,32 @@
+#coding=utf8
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+
+_user = "airgakki@qq.com"
+_pwd = "bzndbiqwunhjbaee"
+_to = "airhaliry@qq.com"
+
+def send():
+    msg = MIMEMultipart('related')
+    msg["Subject"] = "家里情况警报！！！"
+    msg["From"] = _user
+    msg["To"] = _to
+
+    part = MIMEText("树莓派监测到家里有运动物体，附件中是当前家里环境的截图，详细情况请前往网站查看！")
+    msg.attach(part)
+
+    with open('/home/pi/Downloads/stream-video-browser/img/p.jpg','rb') as f:
+        part = MIMEApplication(f.read())
+        part.add_header('Content-Disposition','attachment',filename='fire.jpg')
+        msg.attach(part)
+
+    s = smtplib.SMTP("smtp.qq.com",port=25,timeout=100)
+    s.ehlo()
+    s.starttls()
+    s.login(_user,_pwd)
+    s.sendmail(_user,_to,msg.as_string())
+    s.close()
+
